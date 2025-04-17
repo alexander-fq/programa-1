40999e569a79a2e746279709f4a277bd8b47dc29
cartera_ciberseguridad <alexanderf1492@gmail.com>
2025-04-17 16:13:11 -0500

    java

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Bienvenido");
+        System.out.println("pelicula: Matrix ");
+        
+        int fechaDeLanzamiento = 1999;
+        System.out.println(fechaDeLanzamiento);
+        boolean incluidoEnElPan = true;
+        double notaPelicula = 8.2;
+
+        double promedioPelicula = (8.2 + 6.2 + 4.1) / 3;
+        System.out.println(promedioPelicula);
+
+        String sinopsis = """
+                la pelicula Matrix fue la mejor pelicula del fin del milenio 
+                fue lanzada en:
+                """ + fechaDeLanzamiento;
+
+        System.out.println(sinopsis);  
+        
+        
+    }
+    
+}  
