bf23d63b461a9dd974641f7775ee6586731df8b6
alexanderf-fq <alexanderf1492@gmailcom>
2025-04-17 18:50:23 -0500

    loops

---
+++ loops.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        double nota = 0;
+        double mediaNota = 0;
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Escribe la nota de la pelicula Matrix");
+            nota = teclado.nextDouble();
+            mediaNota = mediaNota + nota;
+            
+        }
+    }
+}
